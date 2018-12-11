8679f9ddc97b92abb84cd460aa9660783392faeb
zk <1576336796@qq.com>
2018-12-11 18:04:48 +0800

    Signed-off-by: zk <1576336796@qq.com>

---
+++ Elasticsearch_en/src/main/java/com/thit/elasticsearch/common/ESConstant.java
@@ -0,0 +1,14 @@
+package com.thit.elasticsearch.common;
+
+public class ESConstant {
+	//匹配中文的正则表达式
+	public static final String regex = "^[\u0391-\uFFE5]+$";
+	//字母正则表达式
+	public static final String regex_word = "[a-zA-z]";
+	//语言标记英文
+	public static final String lang_en = "en";
+	//语言标记中文
+	public static final String lang_zh = "zh";
+	//语言标记中英文
+	public static final String lang_all = "all";
+}
